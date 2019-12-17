02ab8c80a3f16297ee644eb68400e0ee8e45666b
Le PAN <lpan@e3r4p11.42.fr>
2019-12-17 13:40:36 +0100

    setup database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'rootroot',
