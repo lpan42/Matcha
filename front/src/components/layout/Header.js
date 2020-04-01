29b04007c7e1f479c05d1d7ef9cd2a48eb0a56d7
Le Pan <ashley@Les-MacBook-Air.local>
2020-04-01 15:58:43 +0200

    optimize some UI

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -1,5 +1,5 @@
 //rce from es7 react extension
-import React, { Fragment, useContext,useEffect } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';//shortcut impt 
 import { Link } from 'react-router-dom';//import from default does not need {}
 import UserContext from '../../contexts/user/userContext';
@@ -7,6 +7,23 @@ import NotifContext from '../../contexts/notification/notifContext';
 import ProfileContext from '../../contexts/profile/profileContext';
 import NotifBadge from '../badges/NotifBadge';
 import ImageAvatars from '../badges/ImageAvatars';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    paper: {
+      marginRight: theme.spacing(2),
+    },
+  }));
 
 const Header = ({ title }) => {
     const userContext = useContext(UserContext);
@@ -23,6 +40,16 @@ const Header = ({ title }) => {
         }
         //eslint-disable-next-line
     }, [token]);
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const onLogout = () => {
         clearNotif();
@@ -32,13 +59,22 @@ const Header = ({ title }) => {
 
     const authLinks = (
         <Fragment>
-            <li>
+            <Button  ref={anchorRef} onClick={handleToggle}>
                 <ImageAvatars avatarPath={user && user.data.avatar[0].avatar}/>
-                <a href='/notif'><NotifBadge /></a>
-                <a href='/account'>Account</a>
-                <a href={`/profile/${user && user.data.id}`} >Profile</a>
-                <a href="#!" onClick={onLogout}>Logout</a>
-            </li>
+            </Button>
+            <Popper open={open} anchorEl={anchorRef.current}>
+                <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList autoFocusItem={open} >
+                        <MenuItem><a href='/account'>Account</a></MenuItem>
+                        <MenuItem><a href={`/profile/${user && user.data.id}`} >Profile</a></MenuItem>
+                        <MenuItem><a href={`/profile/${user && user.data.id}`} >Friends</a></MenuItem>
+                    </MenuList>
+                    </ClickAwayListener>
+                </Paper>
+            </Popper>
+            <a href='/notif'><NotifBadge /></a>
+            <Button href="#!" onClick={onLogout}><ExitToAppIcon style={{ color:"white" }}/></Button>
         </Fragment>
     )
 
