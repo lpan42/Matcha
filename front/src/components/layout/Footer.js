2c496102653a46d2453fe70574c4a172238e02df
Le Pan <ashley@Les-MacBook-Air.local>
2020-05-06 20:46:57 +0200

    css almost finished

--- front/src/components/layout/Footer.js
+++ front/src/components/layout/Footer.js
@@ -5,17 +5,16 @@ import Typography from '@material-ui/core/Typography';
 const Footer = () => {
     return (
         <footer style={{
-            height:"5%",
+            height:"4vh",
             width:"100%",
-            // position:"fixed",
             bottom: "0", 
             backgroundColor:"var(--primary-color)",
             color:"white",
         }}>
             <Divider />
-            <div style={{display:"flex",justifyContent:"space-between",padding:"0 10px"}}>
-                <Typography variant="caption">Matcha2020</Typography>
-                <Typography variant="caption">Lpan && Cchi @ 42Paris</Typography>
+            <div style={{display:"flex",justifyContent:"space-between", alignItems:"center", padding:"0 10px"}}>
+                <Typography variant="body2">Matcha2020</Typography>
+                <Typography variant="body2">Lpan && Cchi @ 42Paris</Typography>
             </div>
         </footer>
     )
