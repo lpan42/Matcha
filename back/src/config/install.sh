f8391da0123271b6a59855adb39e29a1a3e904a2
c-cchi <cchi@student.42.fr>
2020-05-19 19:50:28 +0200

    update my branch

--- back/src/config/install.sh
+++ back/src/config/install.sh
@@ -2,4 +2,4 @@
 # /Users/lpan/Desktop/MAMP/mysql/bin/mysql -u root --password=rootroot < /Users/lpan/Desktop/Matcha/back/src/config/matcha.sql
 
 # mymac
-/Applications/mamp/mysql/bin/mysql -u root --password=rootroot < /Users/ashley/Desktop/Matcha/back/src/config/matcha.sql
+/Applications/mampstack-5.6.40-1/mysql/bin/mysql -u root --password=123456 < /Users/richard/42/Matcha_github/back/src/config/matcha.sql
