dc6222bd511f40fb5b80581070c2f25831d09b03
c-cchi <cchi@student.42.fr>
2020-05-22 19:08:42 +0200

    add ip to location in file back/modal/user.js

--- back/src/index.js
+++ back/src/index.js
@@ -17,14 +17,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header( 'Access-Control-Allow-Credentials', 'true');
     next();
-  });
-
-app.use(requestIp.mw())
-// app.use(function(req, res) {
-//     // by default, the ip address will be set on the `clientIp` attribute
-//     var IP = req.clientIp;
-//     res.end();
-// });
+});
 
 // include router
 const userRoute = require('./routes/userRoute');
